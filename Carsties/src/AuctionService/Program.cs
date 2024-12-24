7d450b39539e9d92c6a37ff88bcea470a37079c6
Oscar Lagatta <oscarlagatta@gmail.com>
2024-12-24 12:56:51 +0000

    Add consumers and event messaging for Auction lifecycle
    Introduced `AuctionCreated`, `AuctionUpdated`, and `AuctionDeleted` events in Contracts and implemented respective consumers in `SearchService`. Integrated MassTransit with RabbitMQ and configured the Outbox pattern for reliable message delivery in `AuctionService`. Extended mappings and controller logic to publish these events on auction lifecycle changes, ensuring event-driven behavior across services.

--- Carsties/src/AuctionService/Program.cs
+++ Carsties/src/AuctionService/Program.cs
@@ -1,4 +1,7 @@
+using AuctionService.Consumers;
 using AuctionService.Data;
+using Contracts;
+using MassTransit;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +16,46 @@ builder.Services.AddDbContext<AuctionDbContext>(options =>
 
 // Provide Automapper as a service. And specify the location of the assemblies containing the profile classes.
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+// Connection for RabbitMq
+builder.Services.AddMassTransit(x =>
+{
+    /*
+     * Configures an Entity Framework Outbox pattern for the specified AuctionDbContext.
+     * The outbox is useful for managing distributed transactions in a reliable and transactional manner.
+     * 1. x Represents a service collection or configuration instance typically used for dependency injection (e.g., IServiceCollection).
+     * 2. AddEntityFrameworkOutbox<TContext>
+     *  2.1. Adds an Entity Framework Outbox for handling messages inside the AuctionDbContext.
+     *  2.2. The type parameter <AuctionDbContext> specifies the DbContext that will use the outbox pattern.
+     *  3.2. Lambda o => { ... }: This is a configuration delegate where additional properties and behaviors of the outbox can be customized.
+     * 3. Configuration within the Delegate:
+     *  3.1. .QueryDelay
+     *      3.1.1. Specifies a delay for the outbox query in order to handle operations such as polling for unprocessed messages.
+     *      3.1.2. Set to 10 seconds in this case (TimeSpan.FromSeconds(10)), meaning queries for unprocessed outbox messages will occur every 10 seconds.
+     * Use Case
+     * 1. The Outbox Pattern ensures atomicity and durability of messages when integrating with different systems
+     *  through events or messages (e.g., in an event-driven system or microservices architecture).
+     * 2. Using Entity Framework as the outbox storage, it allows applications using AuctionDbContext to safely
+     *  store and forward messages while adhering to database transactions, avoiding inconsistencies.
+     */
+    x.AddEntityFrameworkOutbox<AuctionDbContext>(o =>
+    {
+        o.QueryDelay = TimeSpan.FromSeconds(10);
+
+        o.UsePostgres();
+        o.UseBusOutbox();
+    });
+    
+    x.AddConsumersFromNamespaceContaining<AuctionCreatedFaultConsumer>();
+    
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("auction", false));
+    
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        
+        cfg.ConfigureEndpoints(context);
+    });
+});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 // builder.Services.AddEndpointsApiExplorer();
--- Carsties/src/SearchService/Program.cs
+++ Carsties/src/SearchService/Program.cs
@@ -1,6 +1,9 @@
 using System.Net;
+using Contracts;
+using MassTransit;
 using Polly;
 using Polly.Extensions.Http;
+using SearchService.Consumers;
 using SearchService.Data;
 using SearchService.Services;
 
@@ -10,7 +13,29 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
+
+// Connection for RabbitMq
+builder.Services.AddMassTransit(x =>
+{
+    x.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
+    
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("search", false));
+    
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.ReceiveEndpoint("search-auction-created", e =>
+        {
+            e.UseMessageRetry(r => r.Interval(5, TimeSpan.FromSeconds(5)));
+            
+            e.ConfigureConsumer<AuctionCreatedConsumer>(context);
+        });
+        
+        cfg.ConfigureEndpoints(context);
+    });
+});
 
 var app = builder.Build();
 
